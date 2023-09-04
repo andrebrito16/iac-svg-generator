7e8b661668ecbb806e368ebf4b98073f8bd630ba
Andre Brito <felipe.brito016@gmail.com>
2023-09-04 11:16:27 -0300

    Update to axios

--- packages/action/fetch_user_grades.ts
+++ packages/action/fetch_user_grades.ts
@@ -1,12 +1,13 @@
+import axios from 'axios';
 
 export const fetchUserGrades = async (options: { token: string }) => {
-  const response = await fetch("https://bigdata.insper-comp.com.br/iag/grade_by_exercice/megadados/23-2", {
+  const response = await axios.get("https://bigdata.insper-comp.com.br/iag/grade_by_exercice/megadados/23-2", {
     headers: {
       "Authorization": `Bearer ${options.token}`
     }
   })
 
-  const data = await response.text();
+  const data = await response.data;
 
   return data;
 }
