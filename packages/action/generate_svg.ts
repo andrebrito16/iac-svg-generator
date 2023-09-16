5038fb69dde900fe7081dc69e227119bdd26be42
Andre Brito <felipe.brito016@gmail.com>
2023-09-16 16:44:42 -0300

    Update version of svg generation

--- packages/action/generate_svg.ts
+++ packages/action/generate_svg.ts
@@ -1,34 +1,28 @@
-
-
 export const generateSVG = (grades: Grade[]): string => {
-  const rowHeight = 30;
-  const tableHeight = grades.length * rowHeight + 50;
-  let svg = `<svg width="500" height="${tableHeight}" xmlns="http://www.w3.org/2000/svg" role="img">`;
-  
-  // Background for the entire table
-  svg += `<rect x="0" y="0" width="500" height="${tableHeight}" fill="#f4f4f4" />`;
-  
-  // Table headers
-  svg += `<text x="80" y="25" font-size="16" text-anchor="middle">Atividade</text>`;
-  svg += `<text x="250" y="25" font-size="16" text-anchor="middle">Exercício</text>`;
-  svg += `<text x="400" y="25" font-size="16" text-anchor="middle">Nota</text>`;
-  
-  // Table rows
+  const badgeWidth = 180;
+  const badgeHeight = 22;
+  const gap = 5;
+  const maxSvgWidth = 905;
+  const badgesPerRow = Math.floor(maxSvgWidth / (badgeWidth + gap));
+  const mimimumRows = Math.ceil(grades.length / badgesPerRow);
+  const svgHeight = mimimumRows * (badgeHeight + gap);
+  let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${maxSvgWidth}" height="${svgHeight}">`;
+
   grades.forEach((grade, index) => {
-    const y = index * rowHeight + 50;
-    const gradeColor = grade.grade === 0 ? "red" : "green";
-    
-    // Alternate row background
-    if (index % 2 === 0) {
-      svg += `<rect x="0" y="${y - 20}" width="500" height="20" fill="#e0e0e0" />`;
-    }
-    
-    // Fill background for grade column
-    svg += `<rect x="380" y="${y - 20}" width="40" height="20" fill="${gradeColor}" />`;
-    
-    svg += `<text x="80" y="${y - 5}" font-size="14" text-anchor="middle">${grade.activity}</text>`;
-    svg += `<text x="250" y="${y - 5}" font-size="14" text-anchor="middle">${grade.exercise}</text>`;
-    svg += `<text x="400" y="${y - 5}" font-size="14" text-anchor="middle" fill="white">${grade.grade}</text>`;
+    const row = Math.floor(index / badgesPerRow);
+    const col = index % badgesPerRow;
+    const x = col * (badgeWidth + gap);
+    const y = row * (badgeHeight + gap);
+    const gradeColor = grade.grade === 10 ? '#00b300' : '#ff0000';
+
+    svg += `<rect rx="3" x="${x}" y="${y}" width="${badgeWidth}" height="${badgeHeight}" fill="#595959"/>`;
+    svg += `<rect rx="3" x="${x + badgeWidth * 0.80}" y="${y}" width="${badgeWidth * 0.20}" height="${badgeHeight}" fill="${gradeColor}"/>`;
+    svg += `<rect rx="0" x="${x + badgeWidth * 0.50}" y="${y}" width="${badgeWidth * 0.30}" height="${badgeHeight}" fill="#005ce6"/>`;
+    svg += `<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">`;
+    svg += `<text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.activity}</text><text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.70}">${grade.activity}</text>`;
+    svg += `<text x="${x + badgeWidth * 0.65}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.exercise}</text><text x="${x + badgeWidth * 0.65}" y="${y + badgeHeight * 0.70}">${grade.exercise}</text>`;
+    svg += `<text x="${x + badgeWidth * 0.90}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.grade}</text><text x="${x + badgeWidth * 0.90}" y="${y + badgeHeight * 0.70}">${grade.grade}</text>`;
+    svg += `</g>`;
   });
 
   svg += `</svg>`;
