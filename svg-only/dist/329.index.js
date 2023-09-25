66a0fff55b51950b308ebf92826a1547b0e8dce1
Andre Brito <felipe.brito016@gmail.com>
2023-09-25 08:50:04 -0300

    feat: added truncate

--- svg-only/dist/329.index.js
+++ svg-only/dist/329.index.js
@@ -25,11 +25,15 @@ const generateSVG = (grades) => {
         const x = col * (badgeWidth + gap);
         const y = row * (badgeHeight + gap);
         const gradeColor = grade.grade === 10 ? '#00b300' : '#ff0000';
+        let activityText = grade.activity;
+        if (activityText.length > 20) {
+            activityText = activityText.substring(0, 17) + "...";
+        }
         svg += `<rect rx="3" x="${x}" y="${y}" width="${badgeWidth}" height="${badgeHeight}" fill="#595959"/>`;
         svg += `<rect rx="3" x="${x + badgeWidth * 0.80}" y="${y}" width="${badgeWidth * 0.20}" height="${badgeHeight}" fill="${gradeColor}"/>`;
         svg += `<rect rx="0" x="${x + badgeWidth * 0.50}" y="${y}" width="${badgeWidth * 0.30}" height="${badgeHeight}" fill="#005ce6"/>`;
         svg += `<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">`;
-        svg += `<text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.activity}</text><text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.70}">${grade.activity}</text>`;
+        svg += `<text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${activityText}</text><text x="${x + badgeWidth * 0.25}" y="${y + badgeHeight * 0.70}">${grade.activity}</text>`;
         svg += `<text x="${x + badgeWidth * 0.65}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.exercise}</text><text x="${x + badgeWidth * 0.65}" y="${y + badgeHeight * 0.70}">${grade.exercise}</text>`;
         svg += `<text x="${x + badgeWidth * 0.90}" y="${y + badgeHeight * 0.75}" fill="#010101" fill-opacity=".3">${grade.grade}</text><text x="${x + badgeWidth * 0.90}" y="${y + badgeHeight * 0.70}">${grade.grade}</text>`;
         svg += `</g>`;
